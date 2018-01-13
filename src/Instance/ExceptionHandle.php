36ccf3af5f9c2670401cb23f40edc64409eac28d
evalor <mipone@foxmail.com>
2018-01-13 19:43:14 +0800

    测试版本更新

--- src/Instance/ExceptionHandle.php
+++ src/Instance/ExceptionHandle.php
@@ -5,6 +5,10 @@ namespace EasySwoole\Whoops\Instance;
 use EasySwoole\Core\Http\AbstractInterface\ExceptionHandlerInterface;
 use EasySwoole\Core\Http\Request;
 use EasySwoole\Core\Http\Response;
+use EasySwoole\Whoops\EasyWhoops;
+use Whoops\Exception\Inspector;
+use Whoops\Handler\JsonResponseHandler;
+use Whoops\Handler\PrettyPageHandler;
 
 /**
  * Class ExceptionHandle
@@ -13,9 +17,20 @@ use EasySwoole\Core\Http\Response;
  */
 class ExceptionHandle implements ExceptionHandlerInterface
 {
+    /* @var EasyWhoops $Whoops */
+    protected $Whoops;
 
     public function handle(\Throwable $throwable, Request $request, Response $response)
     {
-        var_dump('is ExceptionHandle');
+        // Call Whoops ExceptionHandle
+        \EasySwoole\Whoops\Instance\Request::getInstance($request);
+        \EasySwoole\Whoops\Instance\Response::getInstance($response);
+
+        $this->Whoops->handleException($throwable);
+    }
+
+    public function setWhoops(EasyWhoops $whoops)
+    {
+        $this->Whoops = $whoops;
     }
 }
